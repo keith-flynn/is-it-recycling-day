988b0f6fe5af39ca10289859314c3bea00be8a31
Keith Flynn <flynn.keith@gmail.com>
2024-01-10 22:02:13 -0500

    Add headers to bypass Mod_Security
    Add headers and now my 406 is a 200 OK!

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
-print("Hello whurld.")
+# Headers to mimic a real user's request
+# The website really didn't like me before adding this
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1'
+}
+
+url = 'https://daytonky.com/calendar-events/'
+page = requests.get(url, headers=headers)
+
+# a test
+print(f"Response Status Code: {page.status_code}")
+
+#print(BeautifulSoup(page.text, 'html'))
+
