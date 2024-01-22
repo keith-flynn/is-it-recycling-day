6478d613cc30ea6871135faa4b436d943dc68581
Keith Flynn <flynn.keith@gmail.com>
2024-01-22 15:58:37 -0500

    Encapsulate email.py code into EmailSender class
    Refactor code to encapsulate email functionality in separate class.

--- email.py
+++ email.py
@@ -3,23 +3,25 @@ import os
 import smtplib
 from email.mime.text import MIMEText
 
-# Load environment variables from .env
-load_dotenv()
+class EmailSender:
+    def __init__(self):
+        load_dotenv()
+        self.sender_email = os.environ.get("GMAIL_USER")
+        self.receiver_email = os.environ.get("GMAIL_RECEIVER")
+        self.password = os.environ.get("GMAIL_PASSWORD")
 
-def send_email(subject, body):
-    sender_email = os.environ.get("GMAIL_USER")
-    receiver_email = os.environ.get("GMAIL_RECEIVER")
-    password = os.environ.get("GMAIL_PASSWORD")
+    def send_email(self, subject, body):
+        message = MIMEText(body)
+        message["Subject"] = subject
+        message["From"] = self.sender_email
+        message["To"] = self.receiver_email
 
-    message = MIMEText(body)
-    message["Subject"] = subject
-    message["From"] = sender_email
-    message["To"] = receiver_email
-
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(self.sender_email, self.password)
+            server.sendmail(self.sender_email, self.receiver_email, message.as_string())
 
 # Example usage
-send_email("Recycling Alert", "Don't forget to put out the recycling tomorrow!")
+if __name__ == "__main__":
+    email_sender = EmailSender()
+    email_sender.send_email("Recycling Alert", "Don't forget to put out the recycling tomorrow!")
