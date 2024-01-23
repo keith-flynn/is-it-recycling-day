4e712438204c88c84fe71b49420941be73e29127
Keith Flynn <flynn.keith@gmail.com>
2024-01-23 15:22:51 -0500

    Separate SQLite code into its own module
    Separation of concerns and readability. All operations that have to do
    with the database will be in database/sqlite_manager.py.

---
+++ database/sqlite_manager.py
@@ -0,0 +1,51 @@
+import sqlite3
+import pandas as pd
+from datetime import datetime
+from email.email_sender import EmailSender
+
+class DBManager:
+    # Add data to Pandas dataframe
+    recycling_day_info = []
+
+    for day in EmailSender.recycling_days:
+        day_text = day.text.strip()
+        date_str = day['data-mec-cell']
+
+        # Convert date string to datetime object
+        date_obj = datetime.strptime(date_str, "%Y%m%d")
+
+        link = day.find('a')['href']
+        recycling_day_info.append({'Date': date_obj, 'Day': day_text, 'Link': link})
+
+    df = pd.DataFrame(recycling_day_info)
+    print(df)
+
+    # SQLite database file
+    db_file = 'recycling_schedule.db'
+
+    # Add data to SQLite database
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+
+    # Create a table if it doesn't exist
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS recycling_schedule (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            day TEXT,
+            date TEXT,
+            link TEXT
+        )
+    ''')
+
+    # Insert data into the table
+    for index, row in df.iterrows():
+        cursor.execute('''
+            INSERT INTO recycling_schedule (day, date, link)
+            VALUES (?, ?, ?)
+        ''', (str(row['Date']), row['Day'], row['Link']))
+
+    # Commit changes and close the connection
+    conn.commit()
+    conn.close()
+
+    print("Data added to SQLite database.")
