f9d11c183616f00002aca524e96a08939c10f44f
Keith Flynn <flynn.keith@gmail.com>
2024-01-26 15:57:15 -0500

    Update main.py to establish current date
    This sets up the logic to check for an upcoming recycling day.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from my_scraper.my_scraper import Scraper
 from database.sqlite_manager import DBManager
 from email_sender.email_sender import EmailSender
@@ -10,6 +11,12 @@ email_sender = EmailSender()
 
 # Database
 db_manager = DBManager(email_sender)
+
+# Get today's date
+today = datetime.now().date()
+
+# Get the date for the next day
+next_day = today + timedelta(days=1)
 
 # Check conditions for sending email
 if conditions_met:
